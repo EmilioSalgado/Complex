94a90ca81412b7214db6c8b6bab844a7d25b13f7
Emilio Salgado <salgado_villagomez_emilio@hotmail.com>
2016-08-31 17:52:54 -0500

    Add complex substraction

---
+++ src/complex_ug/Complex.java
@@ -0,0 +1,39 @@
+package complex_ug;
+
+public class Complex {
+	
+	private double real;
+	private double img;
+	
+	public Complex(){
+	}
+	
+	public Complex(double r, double i){
+		this.real = r;
+		this.img = i;
+	}
+	
+	public double getReal() {
+		return real;
+	}
+	public void setReal(double real) {
+		this.real = real;
+	}
+	public double getImg() {
+		return img;
+	}
+	public void setImg(double img) {
+		this.img = img;
+	}
+	
+	public void sum(Complex a, Complex b){
+		this.real = a.getReal() + b.getReal();
+		this.img = a.getImg() + b.getImg();
+	}
+	
+	public void sub(Complex a, Complex b){
+		this.real = a.getReal() - b.getReal();
+		this.img = a.getImg() - b.getImg();
+	}
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import complex_ug.Complex;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Complex complejo1 = new Complex(3,2);
+		Complex complejo2 = new Complex(5,10);
+		Complex complejo3 = new Complex(0,0);
+		Complex complejo4 = new Complex(0,0);
+		
+		complejo3.sum(complejo1, complejo2);
+		complejo4.sub(complejo1, complejo2);
+		
+		System.out.println("Suma: Real = " + complejo3.getReal() + " " + "Img = " + complejo3.getImg());
+		System.out.println("Resta: Real = " + complejo4.getReal() + " " + "Img = " + complejo4.getImg());
+	}
+
+}
